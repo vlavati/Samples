29d7b9e3d59ad5d6b281c3f62c1b84b973da7f66
vlavati <vlavati>
2021-02-15 21:58:23 +0300

    Added simulated annealing implementation.

---
+++ Samples/SimulatedAnnealing/Annealing.h
@@ -0,0 +1,138 @@
+﻿#pragma once
+
+#include <functional>
+#include <random>
+#include <type_traits>
+
+namespace algorithm
+{
+	template <typename Value = float>
+	struct UniformDistribution
+	{
+		std::default_random_engine generator;
+		std::uniform_real_distribution<Value> distribution{ 0.0f, 1.0f };
+
+		Value operator()()
+		{
+			return distribution(generator);
+		}
+	};
+
+	template <typename Energy = float,
+		typename Temperature = Energy,
+		typename Probability = float>
+	struct ApproximatedAcception
+	{
+		static Probability Get(Energy delta, Temperature temperature)
+		{
+			return exp(-delta / temperature);
+		}
+	};
+
+	template <typename Temperature = float,
+		int InitialTemperature = 10,
+		int ConstProcent = 99>
+	struct SimulatedQuenching
+	{
+		static constexpr Temperature Initial = InitialTemperature;
+		static constexpr Temperature Const = ConstProcent/100.f;
+
+		static Temperature Get(Temperature previous, int iteration)
+		{
+			return previous * Const;
+		}
+	};
+
+	template <typename Temperature = float,
+		int InitialTemperature = 10>
+	struct BoltzmanAnnealing
+	{
+		static constexpr Temperature Initial = InitialTemperature;
+
+		static Temperature Get(Temperature previous, int iteration)
+		{
+			return Initial / (Temperature)log(2 + iteration);
+		}
+	};
+
+	template <typename Temperature = float,
+		int InitialTemperature = 10>
+	struct CauchyAnnealing
+	{
+		static constexpr Temperature Initial = InitialTemperature;
+
+		static Temperature Get(Temperature previous, int iteration)
+		{
+			return Initial / (1 + iteration);
+		}
+	};
+
+	template <
+		typename TemperatureLaw = SimulatedQuenching<>,
+		typename State,
+		typename Energy = float,
+		typename Temperature = Energy,
+		typename Mutation = std::function<State(const State&, Temperature)>,
+		typename CalcEnergy = std::function<Energy(const State&)>,
+		typename ProbeLaw = UniformDistribution<>,
+		typename AcceptionLaw = ApproximatedAcception<>,
+		typename OnIteration = void*
+	>
+	State Annealing(const State& initial,
+		Mutation mutate,
+		CalcEnergy calcEnergy,
+		OnIteration onIteration = nullptr)
+	{
+		ProbeLaw probeLaw;
+
+		const int MaxIteration = 1000000;
+		const Temperature MinimalTemperature = 0;
+
+		State state = initial;
+		Energy energy = calcEnergy(state);
+
+		State minimalState = state;
+		Energy minimalStateEnergy = energy;
+		Temperature temperature = TemperatureLaw::Initial;
+
+		if constexpr (std::is_invocable<OnIteration, int, const State&, Energy, Temperature>::value)
+			onIteration(-1, state, energy, temperature);
+
+		for (int iteration = 0;
+			temperature > MinimalTemperature && iteration < MaxIteration;
+			iteration++)
+		{
+			State newState = mutate(state, temperature);
+			Energy newEnergy = calcEnergy(newState);
+
+			float probability = 1.0;
+			Energy deltaEnergy = newEnergy - energy;
+
+			if (deltaEnergy >= 0.0)
+			{
+				probability = AcceptionLaw::Get(deltaEnergy, temperature);
+			}
+
+			float probe = probeLaw();
+
+			if (probe < probability)
+			{
+				state = newState;
+				energy = newEnergy;
+
+				temperature = TemperatureLaw::Get(temperature, iteration);
+			}
+
+			if (energy < minimalStateEnergy)
+			{
+				minimalStateEnergy = energy;
+				minimalState = state;
+			}
+
+			if constexpr (std::is_invocable<OnIteration, int, const State&, Energy, Temperature>::value)
+				onIteration(iteration, state, energy, temperature);
+		}
+
+		return minimalState;
+	}
+}
---
+++ Samples/SimulatedAnnealing/main.cpp
@@ -0,0 +1,113 @@
+﻿#include "Annealing.h"
+#include <iostream>
+#include <vector>
+
+namespace
+{
+	using ArrayState = std::vector<int>;
+
+	struct UniformPositionMutation
+	{
+		std::uniform_int_distribution<> distribution;
+		UniformPositionMutation(int size, int base = 0, float scale = 0) :
+			distribution(0, size - 1) {}
+	};
+
+	struct CauchyPositionMutation
+	{
+		std::cauchy_distribution<> distribution;
+		CauchyPositionMutation(int size, int base, float scale) :
+			distribution(base, scale) {}
+	};
+
+	struct NormalPositionMutation
+	{
+		std::normal_distribution<> distribution;
+		NormalPositionMutation(int size, int base, float scale) :
+			distribution(base, scale) {}
+	};
+
+	template <typename Temperature = float,
+			typename PositionMutation = UniformPositionMutation>
+	struct Mutation
+	{
+		std::default_random_engine generator;
+
+		ArrayState operator()(const ArrayState& state, Temperature temperature)
+		{
+			ArrayState result = state;
+
+			UniformPositionMutation startPosition((int)result.size());
+
+			int swapPosition1 = startPosition.distribution(generator);
+			int swapPosition2 = swapPosition1;
+
+			PositionMutation positionMutation((int)result.size(), swapPosition1, temperature);
+
+			while (swapPosition2 == swapPosition1 || swapPosition2 < 0 || swapPosition2 >= result.size())
+			{
+				swapPosition2 = (int)positionMutation.distribution(generator);
+			}
+
+			std::swap(result[swapPosition1], result[swapPosition2]);
+
+			return result;
+		}
+	};
+
+	float Energy(const ArrayState& state)
+	{
+		int collisions = 0;
+
+		for (int i = 0; i < state.size() - 1; i++)
+		{
+			for (int j = i + 1; j < state.size(); j++)
+			{
+				if (state[i] > state[j])
+					collisions++;
+			}
+		}
+
+		return (float)collisions;
+	}
+}
+
+struct AcceptedEnergyReached
+{
+	ArrayState state;
+};
+
+int main()
+{
+	std::default_random_engine generator;
+	std::uniform_int_distribution<> dis(1, 100);
+
+	std::vector<int> values(100);
+	std::generate(values.begin(), values.end(), [&]() { return dis(generator); });
+
+	ArrayState initial = values;
+
+    auto onIteration = [](auto iteration, auto& state, auto energy, auto temperature)
+	{
+		std::cout << iteration << ";" << temperature << ";" << energy << std::endl;
+		if (energy == 0)
+			throw AcceptedEnergyReached{ state };
+	};
+
+	ArrayState out;
+
+	try
+	{
+		out = algorithm::Annealing(initial, Mutation<>(), Energy, onIteration);
+	}
+	catch (AcceptedEnergyReached& ex)
+	{
+		out = ex.state;
+	}
+
+	std::sort(initial.begin(), initial.end());
+
+	std::cout << "Result: " << ((out == initial) ? "Ok" : "Fail") << std::endl;
+
+	return 0;
+}
